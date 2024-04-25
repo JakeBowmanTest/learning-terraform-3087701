bd56a47ae7e20a44b4ec2f3f22de0004cfc59f2b
JakeBowmanTest <167910869+JakeBowmanTest@users.noreply.github.com>
2024-04-25 16:25:49 -0500

    changing to curly braces

--- qa/main.tf
+++ qa/main.tf
@@ -1,10 +1,10 @@
 module "qa" {
     source = "../modules/blog"
 
-    environment = (
+    environment = {
         name = "qa"
         network_prefix = "10.1"
-    )
+}
 
     asg_min_size = 0
     asg_max_size = 0
